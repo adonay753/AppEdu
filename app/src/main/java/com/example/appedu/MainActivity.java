6dbfe70390526ac408e9ba74cdedab936d112579
adonay753 <adonayramos19@gmail.com>
2019-10-29 02:01:10 -0400

    casi/listo

--- app/src/main/java/com/example/appedu/MainActivity.java
+++ app/src/main/java/com/example/appedu/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +17,11 @@ public class MainActivity extends AppCompatActivity {
     public void logonboton(View view) {
         Intent i = new Intent(this, SesionIniciada.class);
         startActivity(i);
+        Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
     }
     public void regis (View view){
         Intent i =new Intent(this,RegistroRoles.class);
         startActivity(i);
+        Toast.makeText(this,"Ingresa tus datos y elije tu Rol",Toast.LENGTH_LONG).show();
     }
 }
