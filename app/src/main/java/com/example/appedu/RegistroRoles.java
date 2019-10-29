e4e9b4d4f78131c9225c7737bffa678cf5b04b93
adonay753 <adonayramos19@gmail.com>
2019-10-28 21:37:57 -0400

    hola

---
+++ app/src/main/java/com/example/appedu/RegistroRoles.java
@@ -0,0 +1,23 @@
+package com.example.appedu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RegistroRoles<spinner, Seleccionar, adapterSpinner> extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_roles);
+
+    }
+
+}
