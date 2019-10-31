786997b3da91021882c330c7e275d53c2e846ec2
adonay753 <adonayramos19@gmail.com>
2019-10-30 22:52:55 -0400

    mejorado

--- app/src/main/java/com/example/appedu/mainSesion.java
+++ app/src/main/java/com/example/appedu/mainSesion.java
@@ -1,25 +1,22 @@
 package com.example.appedu;
 
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 
-public class SesionIniciada extends AppCompatActivity {
+public class mainSesion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sesion_iniciada);
+        setContentView(R.layout.activity_main_sesion);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+       FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
