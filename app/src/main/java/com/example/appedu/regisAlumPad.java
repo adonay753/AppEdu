46f8e33e32ccd18f6849a0b0c7a0d8dd3905f9d5
adonay753 <adonayramos19@gmail.com>
2019-11-01 21:05:19 -0400

    Login Listo

--- app/src/main/java/com/example/appedu/regisAlumPad.java
+++ app/src/main/java/com/example/appedu/regisAlumPad.java
@@ -2,7 +2,10 @@ package com.example.appedu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class regisAlumPad extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class regisAlumPad extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regis_alum_pad);
+    }
+    public void regisAlumP (View view) {
+        Intent i= new Intent(this,mainSesion.class);
+        startActivity(i);
+        Toast.makeText(this, "Bienvenido",Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/appedu/regisProf.java
+++ app/src/main/java/com/example/appedu/regisProf.java
@@ -2,7 +2,10 @@ package com.example.appedu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class regisProf extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class regisProf extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regis_prof);
+    }
+    public void regisProfe (View view) {
+        Intent i= new Intent(this,mainSesion.class);
+        startActivity(i);
+        Toast.makeText(this, "Bienvenido",Toast.LENGTH_SHORT).show();
     }
 }
