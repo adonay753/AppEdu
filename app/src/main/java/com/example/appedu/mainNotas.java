0b1d8bedb4eebd74376160d49340e833bfb6883c
dasdo <adonayramos19@gmail.com>
2019-11-14 21:15:07 -0400

    con pruebas

--- app/src/main/java/com/example/appedu/mainNotas.java
+++ app/src/main/java/com/example/appedu/mainNotas.java
@@ -1,7 +1,10 @@
 package com.example.appedu;
 
+import android.content.Intent;
+import android.icu.util.ValueIterator;
 import android.os.Bundle;
 
+import com.example.appedu.Login.registroRoles;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -10,7 +13,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-import com.example.appedu.R;
+
 
 public class mainNotas extends AppCompatActivity {
 
@@ -25,11 +28,17 @@ public class mainNotas extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Subir  Notas", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
             }
         });
+    }
+    public void verNotas (View view){
+
+        Intent i= new Intent(this, verNotas.class);
+        startActivity(i);
+
     }
 
 }
--- app/src/main/java/com/example/appedu/mainSesion.java
+++ app/src/main/java/com/example/appedu/mainSesion.java
@@ -1,11 +1,13 @@
 package com.example.appedu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class mainSesion extends AppCompatActivity {
 
@@ -23,5 +25,9 @@ public class mainSesion extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
+    }
+    public void atajMainNotas (View view) {
+        Intent i = new Intent(this, mainNotas.class);
+        startActivity(i);
     }
 }
