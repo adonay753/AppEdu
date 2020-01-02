a172476c4dbb5ecb75311a1a44388956ec803cd7
dasdo <adonayramos19@gmail.com>
2020-01-02 10:59:25 -0400

    add header

--- app/src/main/java/com/example/appedu/mainSeci.java
+++ app/src/main/java/com/example/appedu/mainSeci.java
@@ -44,14 +44,14 @@ public class mainSeci extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /* FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        }); */
+        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
